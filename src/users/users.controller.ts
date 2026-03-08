258670353e4a46890ef27c66aa27312ca98fe1cf
Tamer Yazar <tamer-yazar@hotmail.com>
2026-03-08 23:02:44 +0300

    feat: tier fix  screen-userid

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -19,7 +19,7 @@ export class UsersController {
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
   async getMe(@Request() req) {
-    return this.usersService.findOne(req.user.id);  // userId → id
+    return this.usersService.findOne(req.user.userId);
   }
   
   @Get()
